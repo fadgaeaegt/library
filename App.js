085eff2828b50e97d8ee7012bff0f2981bb02e99
sungju an <129237908+fadgaeaegt@users.noreply.github.com>
2024-07-04 15:19:48 +0900

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Plot3D from './Plot3D';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>3D Function Plot with Tangent Plane</h1>
+      <Plot3D />
+    </div>
+  );
+}
+
+export default App;
+
